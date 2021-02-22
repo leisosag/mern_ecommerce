e4a9526e30607d67759d0058d51de274d8e0f14e
leisosag <leisosa93@gmail.com>
2021-02-22 18:09:11 -0300

    listado de productos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
 const App = () => {
-  return <div className='App'>Welcome to ProShop</div>;
+  return (
+    <div className='App'>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
